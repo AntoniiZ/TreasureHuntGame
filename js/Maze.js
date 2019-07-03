fc1b1345c7ae3609c99351a374de281ecf1d81d7
Dimitar Stoilov <dks@aeneas.skyaffie.com>
2019-07-03 21:14:45 +0300

    fixed bug

--- js/Maze.js
+++ js/Maze.js
@@ -41,47 +41,27 @@ class Maze extends Phaser.Scene{
       this.setTrap(this.traps, 14, 6);
       this.setTrap(this.traps, 15, 7);
 
-      this.place(this.blocks, 3, 1, 'stone');
-      this.place(this.blocks, 4, 1, 'stone');
-      this.place(this.blocks, 6, 2, 'stone');
-      this.place(this.blocks, 7, 2, 'stone');
-      this.place(this.blocks, 8, 2, 'stone');
-      this.place(this.blocks, 1, 3, 'stone');
-      this.place(this.blocks, 3, 3, 'stone');
-      this.place(this.blocks, 3, 4, 'stone');
-      this.place(this.blocks, 5, 4, 'stone');
-      this.place(this.blocks, 7, 4, 'stone');
-      this.place(this.blocks, 5, 5, 'stone');
-      this.place(this.blocks, 7, 5, 'stone');
-      this.place(this.blocks, 8, 5, 'stone');
-      this.place(this.blocks, 2, 6, 'stone');
-      this.place(this.blocks, 3, 6, 'stone');
-      this.place(this.blocks, 7, 6, 'stone');
-      this.place(this.blocks, 5, 7, 'stone');
-      this.place(this.blocks, 3, 8, 'stone');
-      this.place(this.blocks, 5, 8, 'stone');
-      this.place(this.blocks, 7, 8, 'stone');
-
-      this.place(this.blocks, 10, 1, 'stone');
-      this.place(this.blocks, 15, 1, 'stone');
-      this.place(this.blocks, 12, 2, 'stone');
-      this.place(this.blocks, 10, 3, 'stone');
-      this.place(this.blocks, 12, 3, 'stone');
-      this.place(this.blocks, 14, 3, 'stone');
-      this.place(this.blocks, 10, 4, 'stone');
-      this.place(this.blocks, 14, 4, 'stone');
-      this.place(this.blocks, 15, 4, 'stone');
-      this.place(this.blocks, 13, 5, 'stone');
-      this.place(this.blocks, 14, 5, 'stone');
-      this.place(this.blocks, 16, 6, 'stone');
-      this.place(this.blocks, 9, 7, 'stone');
-      this.place(this.blocks, 10,7, 'stone' );
-      this.place(this.blocks, 12, 7, 'stone');
-      this.place(this.blocks, 14, 7, 'stone');
-      this.place(this.blocks, 16, 7, 'stone');
-      this.place(this.blocks, 16, 8, 'stone');
-
       this.place(this.blocks, 16, 1, 'treasure');
+
+      var field = [
+        [ 0, 0,-1,-1, 0, 0, 0, 0, 0,-1, 0, 0, 0, 0,-1, 0],
+        [ 0, 0, 0, 0, 0,-1,-1,-1, 0, 0, 0,-1, 0, 0, 0, 0],
+        [-1, 0,-1, 0, 0, 0, 0, 0, 0,-1, 0,-1, 0,-1, 0, 0],
+        [ 0, 0,-1, 0,-1, 0,-1, 0, 0,-1, 0, 0, 0,-1,-1, 0],
+        [ 0, 0, 0, 0,-1, 0,-1,-1, 0, 0, 0, 0,-1,-1, 0, 0],
+        [ 0,-1,-1, 0, 0, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0,-1],
+        [ 0, 0, 0, 0,-1, 0, 0, 0,-1,-1, 0,-1, 0,-1, 0,-1],
+        [ 0, 0,-1, 0,-1, 0,-1, 0, 0, 0, 0, 0, 0, 0, 0,-1]
+      ];
+
+      for(let i = 0 ; i < 8; i ++){
+        for(let j = 0; j < 16; j++){
+             if(field[i][j] === -1){
+                 this.place(this.blocks, j+1, i+1, 'stone');
+             }
+         }
+      }
+
 
       this.arr = [];
       this.arr[0] = 96;
